c473fb378fb831e6361600b829f8349e0346f1e3
pamu47 <pamudithamaduranga@gmail.com>
2020-10-29 22:03:48 +0530

    auth action and reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import authReducer from './authReducer';
 import eventReducer from './eventReducer'
 import { combineReducers } from 'redux'
 import { firestoreReducer } from 'redux-firestore'
+import { firebaseReducer } from 'react-redux-firebase'
 
 const rootReducer = combineReducers({
     auth : authReducer,
     event : eventReducer,
-    firestore : firestoreReducer
+    firestore : firestoreReducer,
+    firebase: firebaseReducer
 })
 
 export default rootReducer
